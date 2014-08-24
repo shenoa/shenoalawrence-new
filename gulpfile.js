babe9a1a438b8975a70ddbafb9f93f68859f6622
Shenoa Lawrence <hello@shenoalawrence.com>
2014-08-24 09:26:52 -0700

    added gulp sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass')
 
-gulp.task('default', function() {
-	// default code
+gulp.task('sass', function() {
+	gulp.src('./sass/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./css'));
 });
