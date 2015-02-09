bbb4660da59e4f10dd3805b8b869e58459396960
Shenoa Lawrence <hello@shenoalawrence.com>
2015-02-09 14:34:15 -0800

    big commit for troubleshooting purposes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+// Default task
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("sass/**/*.scss", ['sass']);
+});
 
 gulp.task('sass', function() {
-	gulp.src('./sass/*.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('./css'));
+ 	return gulp.src('sass/**/*.scss')
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(gulp.dest('css'))
+    .pipe(reload({stream:true}))
+		.pipe(notify({ message: 'Styles task complete' }));
+});
+
+// browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('sass/**/*.scss', ['sass']);
+
 });
