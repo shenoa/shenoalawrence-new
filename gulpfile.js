be2cb27cdb471fc0f44c4c028f23d4de9947ff48
Shenoa Lawrence <hello@shenoalawrence.com>
2015-02-25 12:55:13 -0800

    image min resources

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,26 @@ var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var minifyCSS = require('gulp-minify-css');
+
 
 // Default task
 gulp.task('default', ['sass', 'browser-sync'], function () {
     gulp.watch("sass/**/*.scss", ['sass']);
 });
 
+// Compile Sass files
 gulp.task('sass', function() {
- 	return gulp.src('sass/**/*.scss')
+  return gulp.src('sass/**/*.scss')
         .pipe(sass({ style: 'expanded', errLogToConsole: true }))
-		.pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'))
     .pipe(reload({stream:true}))
-		.pipe(notify({ message: 'Styles task complete' }));
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
-// browser-sync task for starting the server.
+// Browser-sync task for starting the server
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
@@ -26,9 +31,32 @@ gulp.task('browser-sync', function() {
     });
 });
 
+// File and folder watching
 gulp.task('watch', function() {
-
-  // Watch .scss files
   gulp.watch('sass/**/*.scss', ['sass']);
-
 });
+
+// Optimization images
+//gulp.task('images' function() {
+//    return gulp.src('src/images/*')
+//        .pipe(imagemin({
+//            progressive: true,
+//            svgoPlugins: [{removeViewBox: false}],
+//            use: [pngquant()]
+//        }))
+//        .pipe(gulp.dest('images'));
+//});
+
+// minify CSS
+//gulp.task('minify-css', function() {
+//   gulp.src('css/*.css')
+//     .pipe(minifyCSS({keepBreaks:true}))
+//     .pipe(gulp.dest('css/'))
+// });
+
+// pre-deploy prep tasks
+//gulp.task('prep', ['imagemin', 'minify-css'], function() {
+//});
+
+// possibly deploy to s3 using https://gist.github.com/michaelcolenso/c9b911c3c3a0cff36e63
+
